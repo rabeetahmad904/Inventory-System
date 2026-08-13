9cab32e320fe8d3b6f8abbcaa442dba00293affc
Rabeet Ahmad <rabeetahmad904@gmail.com>
2026-08-13 16:14:52 +0500

    Add root route and vercel config for backend

--- backend/server.js
+++ backend/server.js
@@ -11,23 +11,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
+app.get('/', (req, res) => {
+  res.send('Inventory API is up and running!');
+});
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/items', require('./routes/itemRoutes'));
 
-// Base Route
+
 app.get('/', (req, res) => {
   res.send('Inventory Management API Running...');
 });
 
-// 404 Handler for Undefined Routes
 app.use((req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 });
 
-// Global Error Handler - MUST have exactly 4 arguments: (err, req, res, next)
+
 app.use((err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode).json({
